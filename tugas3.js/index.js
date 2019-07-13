63bd87e4e3b6cb85bde47efba15eb6abdd830e22
Engkos <engkoskostaman97@gmail.com>
2019-07-14 02:22:25 +0700

    Tugas 3 javascript
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas3.js/index.js
@@ -0,0 +1,14 @@
+function ambil(){
+  /*sebelum dirubah*/
+  return"terambil";
+  /* sesudah dirubah*/
+  var lempar="diterima"
+  return lempar;
+}
+function mintadata(x = 10){
+
+  console.log(x)
+}
+mintadata(20);
+
+console.log (ambil());
